9ea1dc559efb91e68fb233a22c67f4c29753ce6b
hdkef <hdkef11@gmail.com>
2023-02-01 16:15:32 +0700

    add selection sort

--- selectionsort/selectionsort.go
+++ selectionsort/selectionsort.go
@@ -1,9 +1,25 @@
 package selectionsort
 
 func SelectionSortDesc(arr []int) {
-
+	for i := 0; i < len(arr)-1; i++ {
+		idx := i
+		for j := i + 1; j < len(arr); j++ {
+			if arr[j] > arr[idx] {
+				idx = j
+			}
+		}
+		arr[i], arr[idx] = arr[idx], arr[i]
+	}
 }
 
 func SelectionSortAsc(arr []int) {
-
+	for i := 0; i < len(arr)-1; i++ {
+		idx := i
+		for j := i + 1; j < len(arr); j++ {
+			if arr[j] < arr[idx] {
+				idx = j
+			}
+		}
+		arr[i], arr[idx] = arr[idx], arr[i]
+	}
 }
